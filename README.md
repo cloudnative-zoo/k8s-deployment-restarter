2f7353b37446fdacbbc7b68acb4b23554d459ed3
Hassnat <hassnatahmadpk@gmail.com>
2024-05-16 21:09:32 +0200

    fix: changed post to get, improved docs

--- README.md
+++ README.md
@@ -1,27 +1,33 @@
 # k8s-deployment-restarter
 
 ## Requirements
+
     docker
     python > 3.10
     kubectl
 
 ## Local setup
 
-### create a python virtual environement 
+### create a python virtual environement
+
 ```bash
 python -m venv venv
 activate ven `source venv/bin/activate
 pip install -r requirements.txt
 ```
+
 #### To run locally
+
 ```bash
 export KUBECONFIG="/Users/hassnat/.kube/config"
 export API_KEY="secret"
 python main.py
 ```
+
 ## Run in Kubernetes
 
 **make sure you have built and pushed docker image to docker registery to build**
+
 ```bash
 docker buildx build --platform linux/amd64 -t k8s-deployment-restarter:latest .
 ```
@@ -31,18 +37,18 @@ docker buildx build --platform linux/amd64 -t k8s-deployment-restarter:latest .
 #### add image and tag to manifests/app.yaml
 
 ### Apply manifests
+
     kubectl apply -f manifests/
 
-
 ## Test
+
 ```bash
-curl -X 'POST' \
-  'http://0.0.0.0:8080/restart-deployment/?api_key=secret' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "namespace": "edyou-dev-apps",
-  "deployment": "notification-worker-deployment"
-}'
+curl -X 'GET' 'http://localhost:8080/restart-deployment/YourNAMESPACE_NAME/YourDEPLOYMENT_NAME?api_key=YourAPIKEY'
+```
+
+### or open in browser
+
+```bash
+http://localhost:8080/restart-deployment/YourNAMESPACE_NAME/YourDEPLOYMENT_NAME?api_key=YourAPIKEY
 ```
 
--- main.py
+++ main.py
@@ -1,40 +1,33 @@
 import datetime
-
-from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel
-from kubernetes import client, config
-from typing import Optional
 import os
 
+from fastapi import FastAPI, HTTPException, Depends
+from kubernetes import client, config
+
 # Load Kubernetes config based on environment
-config.load_kube_config() if os.getenv('KUBECONFIG') else config.load_incluster_config()
+config.load_kube_config(context="do-nyc3-hassnat-k8s") if os.getenv('KUBECONFIG') else config.load_incluster_config()
 
 app = FastAPI()
 
 
-class DeploymentAction(BaseModel):
-    namespace: str
-    deployment: str
-
-
-def get_api_key(api_key: Optional[str] = None):
+def get_api_key(api_key: str):
     if api_key != os.getenv("API_KEY"):
         raise HTTPException(status_code=403, detail="Unauthorized. Please provide correct api_key")
     return api_key
 
 
-@app.post("/restart-deployment/")
-async def restart_deployment(deployment_action: DeploymentAction, api_key: str = Depends(get_api_key)):
+@app.get("/restart-deployment/{namespace_name}/{deployment_name}")
+async def restart_deployment(namespace_name: str, deployment_name: str, api_key: str = Depends(get_api_key)):
     apps_v1 = client.AppsV1Api()
     try:
         deployment = apps_v1.read_namespaced_deployment(
-            name=deployment_action.deployment, namespace=deployment_action.namespace)
+            name=deployment_name, namespace=namespace_name)
         if deployment:
             # Restart deployment
             deployment.spec.template.metadata.annotations = {
                 "kubectl.kubernetes.io/restartedAt": datetime.datetime.now(datetime.UTC).isoformat()}
             res = apps_v1.patch_namespaced_deployment(
-                name=deployment_action.deployment, namespace=deployment_action.namespace, body=deployment)
+                name=deployment_name, namespace=namespace_name, body=deployment)
             return {"status": "success", "message": "Deployment restarted successfully"}
     except client.exceptions.ApiException as e:
         if e.status == 404:
@@ -48,4 +41,4 @@ async def restart_deployment(deployment_action: DeploymentAction, api_key: str =
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8081)
